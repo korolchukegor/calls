1279f42d8a13700c9144db6372067328c84e9314
Egor <egorkorolchuk@gmail.com>
2016-03-22 14:00:40 +0300

    email works

--- send_email.py
+++ send_email.py
@@ -1,36 +1,46 @@
 # coding: utf-8
+
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
 
-
-#Параметр, передаваемый при запуске скрипта:
-txtparam = 'TESTTESTTEST'
-#От кого:
+subject = u'Тестирование оповещений на email'
 fromaddr = 'Mr. Robot <korolchukwork@gmail.com>'
-#Кому:
 toaddr = 'Administrator <egorkorolchuk@gmail.com>'
-#Тема письма:
-subj = 'Notification from system'
-#Текст сообщения:
-msg_txt = 'Notice:\n\n ' +  txtparam + '\n\nBye!' #
-#Создаем письмо (заголовки и текст)
-msg = "From: %s\nTo: %s\nSubject: %s\n\n%s"  % ( fromaddr, toaddr, subj, msg_txt)
 
-#Логин gmail аккаунта. Пишем только имя ящика.
-#Например, если почтовый ящик someaccount@gmail.com, пишем:
+multipart = MIMEMultipart('alternative')
+multipart['Subject'] = Header(subject.encode('utf-8'), 'UTF-8').encode()
+multipart['To'] = Header(toaddr.encode('utf-8'), 'UTF-8').encode()
+multipart['From'] = Header(fromaddr.encode('utf-8'), 'UTF-8').encode()
+
+html = u'<html><body>Привет, {}!</br>Это тестовая рассылка!</body></html>'.format(toaddr)
+text = u'Привет!\nЭто тестовая рассылка!'
+
+htmlpart = MIMEText(html.encode('utf-8'), 'html', 'UTF-8')
+multipart.attach(htmlpart)
+textpart = MIMEText(text.encode('utf-8'), 'plain', 'UTF-8')
+multipart.attach(textpart)
+
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+multipart.attach(part1)
+multipart.attach(part2)
+
 username = u'korolchukwork@gmail.com'
-#Соответственно, пароль от ящика:
 password = u'wgBZ1FgI8ykmWNF8vQgP'
-serv = u'smtp.gmail.com:587'
 #Инициализируем соединение с сервером gmail по протоколу smtp.
+serv = u'smtp.gmail.com:587'
 server = smtplib.SMTP(serv)
 #Выводим на консоль лог работы с сервером (для отладки)
-server.set_debuglevel(1);
+# server.set_debuglevel(1);
 #Переводим соединение в защищенный режим (Transport Layer Security)
 server.starttls()
 #Проводим авторизацию:
 server.login(username,password)
 #Отправляем письмо:
-server.sendmail(fromaddr, toaddr, msg)
+server.sendmail(fromaddr, toaddr, multipart.as_string())
 #Закрываем соединение с сервером
 server.quit()
 
