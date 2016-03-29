9a5c329817ec696c742af7f681dea352cd06010d
Egor <egorkorolchuk@gmail.com>
2016-03-29 13:36:13 +0300

    defs and config

--- calls.py
+++ calls.py
@@ -12,61 +12,35 @@ def read_func(file):
     dop = []
     zch = []
     ins = []
+    int_serv_nums = []
+    int_sales_nums = []
+    int_tradein_nums = []
+    int_nfz_nums = []
+    int_dop_nums = []
+    int_zch_nums = []
+    int_ins_nums = []
 
-    int_serv_nums = [int(nums) for nums in [
+    def read_config(int_dept_nums, colnum):
+        with open('config.csv', newline='') as csvfile:
+            cells = csv.reader(csvfile, delimiter=';', quotechar='|')
+            next(cells)
 
-        519,
-        530,
-        534,
-        535,
-        536,
-        538,
-        2447778
-    ]]
-    int_sales_nums = [int(nums2) for nums2 in [
+            for cell in cells:
+                if cell[colnum] != '':
+                    int_dept_nums.append(int(cell[colnum]))
 
-        520,
-        521,
-        522,
-        523,
-        525,
-        526
-    ]]
-    int_tradein_nums = [int(nums3) for nums3 in [
-
-        513
-    ]]
-    int_nfz_nums = [int(nums3) for nums3 in [
-
-        514
-    ]]
-    int_dop_nums = [int(nums3) for nums3 in [
-
-        545
-    ]]
-    int_zch_nums = [int(nums3) for nums3 in [
-
-        551,
-        553,
-        554,
-        555
-    ]]
-    int_ins_nums = [int(nums4) for nums4 in [
-
-        562,
-        563,
-        564,
-        567,
-        568
-    ]]
-
+    read_config(int_serv_nums, 0)
+    read_config(int_sales_nums, 1)
+    read_config(int_tradein_nums, 2)
+    read_config(int_nfz_nums, 3)
+    read_config(int_dop_nums, 4)
+    read_config(int_zch_nums, 5)
+    read_config(int_ins_nums, 6)
 
     def check_phone(int_dept_nums, dept, time):
 
         if i[1] == 'RX' and int(i[3]) in int_dept_nums and int(i[4]) >= time:
             dept.append(i[2])
-
-
 
     with open('{}'.format(file), newline='') as csvfile:
         callsreader = csv.reader(csvfile, delimiter=';', quotechar='|')
@@ -80,13 +54,12 @@ def read_func(file):
             check_phone(int_zch_nums, zch, 45)
             check_phone(int_ins_nums, ins, 45)
 
-
     global html_text
     with open('template_1.html', 'r', encoding='utf-8') as h:
         html_text = h.read().format(len(serv), len(set(serv)),
-                       len(sales), len(set(sales)),
-                       len(nfz), len(set(nfz)),
-                       len(tradein), len(set(tradein)),
-                       len(dop), len(set(dop)),
-                       len(zch), len(set(zch)),
-                       len(ins), len(set(ins)))
+                                    len(sales), len(set(sales)),
+                                    len(nfz), len(set(nfz)),
+                                    len(tradein), len(set(tradein)),
+                                    len(dop), len(set(dop)),
+                                    len(zch), len(set(zch)),
+                                    len(ins), len(set(ins)))
